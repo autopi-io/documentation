34276bbd192de2b7b31c6ec0338c55dd40f3fae5
Henrik Nilsson <henrik@autopi.io>
2021-01-04 14:52:52 +0100

    Renamed 'content' folder back to 'docs'

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import styles from './styles.module.css';
 import { Redirect } from "@docusaurus/router";
 
 const Index = () => {
-  return <Redirect to="/documentation/content/" />;
+  return <Redirect to="/documentation/" />;
 };
 
 export default Index;
