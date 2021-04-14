9cd205987cf3bd7c3a333ccdf9fb9974f0a09be4
Nikola Velichkov <nikola@autopi.io>
2021-04-14 14:49:00 +0200

    Make more SEO optimisation changes

--- src/theme/Layout.jsx
+++ src/theme/Layout.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import OriginalLayout from '@theme-original/Layout';
+import Head from '@docusaurus/Head';
 import {Redirect, useLocation} from '@docusaurus/router';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 const AddTrailingSlashRedirect = () => {
   const location = useLocation();
@@ -11,10 +13,23 @@ const AddTrailingSlashRedirect = () => {
 };
 
 export default function Layout(props) {
+  const { siteConfig } = useDocusaurusContext();
+  const location = useLocation();
+
   return (
     <>
       <AddTrailingSlashRedirect />
       <OriginalLayout {...props} />
+      <Head>
+        <meta
+          property="og:url"
+          content={`${siteConfig.url}${location.pathname}`}
+        />
+        <link
+          rel="canonical"
+          href={`${siteConfig.url}${location.pathname}`}
+        />
+      </Head>
     </>
   );
 }
