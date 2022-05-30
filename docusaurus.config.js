8ee7307d17a4c43a9f58712a700a33240ab37d81
Dragos Ionescu <dragos@autopi.io>
2022-05-30 15:28:46 +0200

    Revert "Upgraded peer pedendencies and updated package-lock.json"
    This reverts commit 15ad54da929fa53ce6e713ae0fb86f80e9903d82.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,13 +10,17 @@ module.exports = {
   trailingSlash: true,
   themeConfig: {
     image: 'img/favicon-194x194.png',
-    metadata: [
+    metadatas: [
       { property: 'og:type', content: 'website' },
     ],
     algolia: {
       appId: '7BRH96O31B',
       apiKey: '6dfef04434e917765e1dd4f89e170e74',
       indexName: 'autopi',
+    },
+    gtag: {      
+      trackingID: 'G-DB2BZPKYN9',      
+      anonymizeIP: false,
     },
     navbar: {
       logo: {
@@ -117,10 +121,6 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: '/',
           editUrl: "https://github.com/autopi-io/documentation/edit/master/"
-        },
-        gtag: {      
-          trackingID: 'G-DB2BZPKYN9',      
-          anonymizeIP: false,
         },
         theme: {
           customCss: require.resolve('./src/css/custom.scss'),
