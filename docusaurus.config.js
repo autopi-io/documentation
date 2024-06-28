f4330b98ad3ba546721b79d2487b31e31079daa7
Lasse Andersen <lan@autopi.io>
2024-06-28 14:42:03 +0200

    Added demo enviroment links and added uniform treatment of links in the footer.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -94,6 +94,10 @@ module.exports = {
               to: 'https://my.autopi.io',
             },
             {
+              label: 'AutoPi Demo Environment',
+              to: 'https://demo.autopi.io'
+            },
+            {
               label: 'Shop',
               to: 'https://shop.autopi.io',
             },
@@ -108,12 +112,12 @@ module.exports = {
           items: [
             {
               label: 'AutoPi Community',
-              href: 'https://community.autopi.io/',
+              to: 'https://community.autopi.io/',
             },
             {
               label: 'GitHub - AutoPi Core',
-              href: 'https://github.com/autopi-io',
-            },            
+              to: 'https://github.com/autopi-io',
+            },
           ],
         },
         {
@@ -121,23 +125,23 @@ module.exports = {
           items: [           
             {
               label: 'Reddit',
-              href: 'https://www.reddit.com/r/autopi/',
+              to: 'https://www.reddit.com/r/autopi/',
             },
             {
               label: 'Twitter',
-              href: 'https://twitter.com/autopi_io',
+              to: 'https://twitter.com/autopi_io',
             },
             {
               label: 'Facebook',
-              href: 'https://www.facebook.com/autopi.io/',
+              to: 'https://www.facebook.com/autopi.io/',
             },
             {
               label: 'LinkedIn',
-              href: 'https://www.linkedin.com/company/autopi.io',
+              to: 'https://www.linkedin.com/company/autopi.io',
             },
             {
               label: 'Youtube',
-              href: 'https://www.youtube.com/channel/UCH50VR17emPVVfnIWlGMLig',
+              to: 'https://www.youtube.com/channel/UCH50VR17emPVVfnIWlGMLig',
             },
           ],
         },
